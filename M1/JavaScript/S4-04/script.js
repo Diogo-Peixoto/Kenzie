43c12ae647c1a50ffee1eda4619a127c334c6454
Diogo Soares Peixoto <71359547+Diogo-Peixoto@users.noreply.github.com>
2022-06-20 11:19:06 -0300

    .

---
+++ M1/JavaScript/S4-04/script.js
@@ -0,0 +1,20 @@
+function somar(valor1, valor2){
+    return console.log(valor1 + valor2);
+}
+
+
+function valor(valor){
+    console.log(`O valor (${valor}) é do tipo (${typeof(valor)})`)
+}
+
+
+
+function somarNotas(nota1, nota2, nota3, nota4){
+    if(typeof(nota1) == 'number' && typeof(nota2) == 'number' && typeof(nota3) == 'number' && typeof(nota4) == 'number' ){
+        return console.log(nota1+nota2+nota3+nota4)
+    }else{
+        return console.log("O campo só pode receber números")
+    }
+}
+
+somarNotas(2,2,2,2)
