e9e143738a2909ddea99e7e0608a92063e170a39
Diogo Soares Peixoto <71359547+Diogo-Peixoto@users.noreply.github.com>
2022-06-21 13:36:53 -0300

    .

---
+++ M1/JavaScript/S4-09/index.js
@@ -0,0 +1,9 @@
+let nomeDosCompetidores = ["Maria", "Heric", "Roberta", "Rafael", "Lucas"]
+
+
+console.log(nomeDosCompetidores.shift());
+console.log(nomeDosCompetidores.pop());
+
+for(let i = 0; i < nomeDosCompetidores.length;i++){
+    console.log(nomeDosCompetidores[i])
+}
