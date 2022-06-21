bc7dc20b949ab2241f0b787bf3bbc7c400ac5865
Diogo Soares Peixoto <71359547+Diogo-Peixoto@users.noreply.github.com>
2022-06-21 13:59:44 -0300

    .

--- M1/JavaScript/S4-09/index.js
+++ M1/JavaScript/S4-09/index.js
@@ -1,9 +1,29 @@
 let nomeDosCompetidores = ["Maria", "Heric", "Roberta", "Rafael", "Lucas"]
 
 
-console.log(nomeDosCompetidores.shift());
-console.log(nomeDosCompetidores.pop());
+//console.log(nomeDosCompetidores.shift());
+//console.log(nomeDosCompetidores.pop());
 
 for(let i = 0; i < nomeDosCompetidores.length;i++){
-    console.log(nomeDosCompetidores[i])
+    //console.log(nomeDosCompetidores[i])
 }
+
+//-------------------------------------------------------
+
+function testaPosicao(lista, posicao){
+    for(let i = 0;i < lista.length;i++){
+        if(posicao == 0 && lista[i] == "Vou ser Dev"){
+            return //console.log("Ganhou");
+        }
+    }
+    return console.log("Não ganhou")
+}
+
+let carrinhoDeCompras =
+    [
+        "Funko Pop Kakashi",
+        "Dragon Ball Collection",
+        "Vou ser Dev"
+    ]
+testaPosicao(carrinhoDeCompras, 0)
+
